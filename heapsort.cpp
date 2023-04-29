db0fe254a210908babbac318df10cd1499c63c0c
Ayleen Roque <118084437+IcedPeppermintTea@users.noreply.github.com>
2023-04-29 18:11:21 -0400

    Update heapsort.cpp

--- heapsort.cpp
+++ heapsort.cpp
@@ -2,33 +2,33 @@
 
 const int SIZE = 8; //the length of the array 
 
-    void printArray(int x[]){
+    void printArray(int x[]){ // parameter: array
 
-		for(int i=0;i<SIZE;i++){
-			std::cout<< x[i] << " ";
+		for(int i=0;i<SIZE;i++){ // traverse while i is less than the size of the array
+			std::cout<< x[i] << " "; // print each value
 		}
 		std::cout<<"\n";
 	}
 
-	void heapsort(int x[]){
+	void heapsort(int x[]){ // parameter: array
 		int i, parent, child;
 
-		if(SIZE == 0)	return;
+		if(SIZE == 0)	return; // if array is empty
 		
 		// preprocessing phase; create initial heap
-		for(i = 1; i < SIZE; i++){
-			int elt = x[i];
-			std::cout <<"bubble-up i=" << i << "  x[i]=" << x[i] << "\n"; 
+		for(i = 1; i < SIZE; i++){ // while i is less than the size of the array
+			int elt = x[i]; // elt = current value at index i 
+			std::cout <<"bubble-up i=" << i << "  x[i]=" << x[i] << "\n"; // print index and value at index 
 
 			// bubble_up
-			child = i;
-			parent = (child - 1)/2;
-			while(child > 0 && x[parent] < elt){
-				x[child] = x[parent];
-				child = parent;
-				parent = (child - 1)/2;
+			child = i; // child = index
+			parent = (child - 1)/2; // parent is (index - 1)/2
+			while(child > 0 && x[parent] < elt){ // while the index is bigger than 0 and the value at index parent is less than elt value
+				x[child] = x[parent]; // value at child becomes value at parent
+				child = parent; // child index become parent index
+				parent = (child - 1)/2; // parent becomes (child index - 1)/2
 			}
-			x[child] = elt;
+			x[child] = elt; // value at child becomes elt value
 		}
 
 		std::cout << "Initial heap: " <<"\t";
@@ -36,15 +36,15 @@ const int SIZE = 8; //the length of the array
 		
 		// selection phase; repeatedly remove x[0], insert it
 		// in its proper position and adjust the heap
-		for(i = SIZE-1; i > 0; i--){
+		for(i = SIZE-1; i > 0; i--){ // while i is the size of the array and bigger than zero, decrement
 			// bubble-down
 
-			std::cout <<"i=" << i << "  x[i]=" << x[i] << "\t"; 
+			std::cout <<"i=" << i << "  x[i]=" << x[i] << "\t";  // print index and value
 			printArray(x);
 			
 			//put x[0] in i-th position
-			int iValue = x[i];
-			x[i] = x[0];
+			int iValue = x[i]; // iValue = value at index i
+			x[i] = x[0]; // value at index i = value at x[0]
 			
 			parent = 0;
 			
